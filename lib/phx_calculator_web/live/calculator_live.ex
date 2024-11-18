968a05544020004eef0b320f8c4dd257b14751ad
MikeRund <mikerundle1999.MR@gmail.com>
2024-11-18 11:44:26 +0000

    changed the calculation function to work with chaining

--- lib/phx_calculator_web/live/calculator_live.ex
+++ lib/phx_calculator_web/live/calculator_live.ex
@@ -4,7 +4,7 @@ alias PhxCalculatorWeb.CoreComponents
   import CoreComponents
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, calc: [], current: "0") |> dbg()
+    socket = assign(socket, calc: [], current: "0", history: [])
     {:ok, socket}
   end
 
@@ -12,16 +12,17 @@ alias PhxCalculatorWeb.CoreComponents
     current =
     case socket.assigns.current do
       "0" -> ""
+      val when is_float(val) -> ""
       _ -> socket.assigns.current
     end
-    socket = assign(socket, current: current <> number) |> dbg()
+    socket = assign(socket, current: current <> number)
     {:noreply, socket}
   end
 
   def handle_event("operation", %{"operation" => operation}, socket) do
     calc = socket.assigns.calc ++ [socket.assigns.current, operation]
     current = ""
-    socket = assign(socket, calc: calc, current: current) |> dbg()
+    socket = assign(socket, calc: calc, current: current)
     {:noreply, socket}
   end
 
@@ -29,39 +30,30 @@ alias PhxCalculatorWeb.CoreComponents
   def handle_event("equals", _unsigned_params, socket) do
     calc = socket.assigns.calc ++ [socket.assigns.current]
     current = ""
-    socket = assign(socket, calc: calc, current: current) |> dbg()
+    socket = assign(socket, calc: calc, current: current)
     {:noreply, socket}
-    calculate(0, socket.assigns.calc, socket)
+
+    [acc | rest] = calc
+    Float.parse(acc)
+    acc = Float.parse(acc) |> elem(0)
+    calculate(acc, rest, socket)
   end
 
   # pass accumulator value and the to-be calculated values
-  defp calculate(_acc, calc, socket) when length(calc) > 0 do
+  defp calculate(acc, calc, socket) when length(calc) > 0 do
 
-    # split calc into lists of 3 (number *(op) number)
-    [num1, op, num2 | rest] = calc
-    num1 = Float.parse(num1) |> elem(0)
+    # obtain the operation and the next number
+    [op, num2 | rest] = calc
     num2 = Float.parse(num2) |> elem(0)
-    acc = apply_operation(num1, op, num2)
+    acc = apply_operation(acc, op, num2)
 
     # call recursively
     calculate(acc, rest, socket)
   end
 
-  # Need to handle case where repeated operations are added so length(calc) = 2
-  # defp calculate(acc, calc, socket) when length(calc) == 2 do
-
-  #   # split calc into lists of 3 (number *(op) number)
-  #   [op, num2 | rest] = calc
-  #   num2 = Float.parse(num2) |> elem(0)
-  #   acc = apply_operation(acc, op, num2)
-
-  #   # call recursively
-  #   calculate(acc, rest, socket)
-  # end
-
   defp calculate(acc, [], socket) do
-    socket = assign(socket, current: acc)
-    {:noreply, socket}
+    socket = assign(socket, current: acc, calc: [])
+    {:noreply, socket} |> dbg()
   end
 
   # helper functions
